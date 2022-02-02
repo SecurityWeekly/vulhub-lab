1dec26c75d12255efe8fe90b3bf21101ec9a9cb5
pasadoorian <paul@securityweekly.com>
2022-02-02 08:29:43 -0500

    Trying to fix HTTP server....

--- http-server/entrypoint.sh
+++ http-server/entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-exec /usr/sbin/sshd -D
+set -e
 
-exec /usr/bin/python3 -m http.server --directory /files 80 &
+# Make sure scripts in /opt/env are usable:
+#export PATH="/opt/env/bin:${PATH}"
+#export LD_LIBRARY_PATH="$/opt/env/lib:${LD_LIBRARY_PATH}"
+
+# Start uwsgi
+/usr/sbin/sshd -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start SSHD: $status"
+  exit $status
+fi
+
+/usr/bin/python3 -m http.server --directory /files 80
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start Python Web Server: $status"
+  exit $status
+fi
