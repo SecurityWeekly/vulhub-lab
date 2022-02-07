da712584f84c141954fb7d4f5a672621bb5911b9
Paul Asadoorian <paul@securityweekly.com>
2022-02-07 12:47:39 -0500

    Working Windows 10 target, integrated into docker-compose.

--- win10/startup.sh
+++ win10/startup.sh
@@ -2,30 +2,26 @@
 set -eou pipefail
 
 chown root:kvm /dev/kvm
-#service libvirtd start
-#service virtlogd start
+
 /usr/sbin/libvirtd --daemon
 /usr/sbin/virtlogd --daemon
+
 VAGRANT_DEFAULT_PROVIDER=libvirt vagrant up
-#iptables-save > $HOME/firewall.txt
-#iptables -X
-#iptables -t nat -F
-#iptables -t nat -X
-#iptables -t mangle -F
-#iptables -t mangle -X
-#iptables -P INPUT ACCEPT
-#iptables -P FORWARD ACCEPT
-#iptables -P OUTPUT ACCEPT
-#
-#iptables -A FORWARD -i eth0 -o virbr1 -p tcp --syn --dport 3389 -m conntrack --ctstate NEW -j ACCEPT
-#iptables -A FORWARD -i eth0 -o virbr1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-#iptables -A FORWARD -i virbr1 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-#iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 3389 -j DNAT --to-destination 192.168.121.68
-#iptables -t nat -A POSTROUTING -o virbr1 -p tcp --dport 3389 -d 192.168.121.68 -j SNAT --to-source 192.168.121.1
-#
-#iptables -D FORWARD -o virbr1 -j REJECT --reject-with icmp-port-unreachable
-#iptables -D FORWARD -i virbr1 -j REJECT --reject-with icmp-port-unreachable
-#iptables -D FORWARD -o virbr0 -j REJECT --reject-with icmp-port-unreachable
-#iptables -D FORWARD -i virbr0 -j REJECT --reject-with icmp-port-unreachable
+
+iptables-save > /root/firewall.txt
+iptables -A LIBVIRT_FWI -i eth0 -o virbr1 -p tcp --syn --dport 3389 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A LIBVIRT_FWI -i eth0 -o virbr1 -p tcp --syn --dport 445 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A LIBVIRT_FWI -i eth0 -o virbr1 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A LIBVIRT_FWI -i virbr0 -o eth0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 3389 -j DNAT --to-destination 192.168.121.10
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 445 -j DNAT --to-destination 192.168.121.10
+iptables -t nat -A LIBVIRT_PRT -o virbr1 -p tcp --dport 3389 -d 192.168.121.10 -j SNAT --to-source 192.168.121.1
+iptables -t nat -A LIBVIRT_PRT -o virbr1 -p tcp --dport 445 -d 192.168.121.10 -j SNAT --to-source 192.168.121.1
+
+iptables -D LIBVIRT_FWI -o virbr1 -j REJECT --reject-with icmp-port-unreachable
+iptables -D LIBVIRT_FWI -o virbr0 -j REJECT --reject-with icmp-port-unreachable
+iptables -D LIBVIRT_FWO -i virbr1 -j REJECT --reject-with icmp-port-unreachable
+iptables -D LIBVIRT_FWO -i virbr0 -j REJECT --reject-with icmp-port-unreachable
 
 exec "$@"
